97c886100cf1d4d87bfe4c38ea3737a33219a7de
Dora Oline Eriksrud <doraoline@gmail.com>
2017-09-17 16:51:07 +0200

    Started to look at implement fileChooser

--- javaApp/src/controllers/AddDatabaseController.java
+++ javaApp/src/controllers/AddDatabaseController.java
@@ -1,8 +1,24 @@
 package controllers;
 
+import java.awt.Desktop;
+
 import Main.MainApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
 public class AddDatabaseController {
+	
+	@FXML
+	private Button createOutputButton;
+	@FXML
+	private FileChooser fileChooserButton;
+	@FXML
+	private TextField loadedFileNameField;
+	
+	FileChooser fileChooser = new FileChooser();
+	
 	
 	private MainApp mainApp;
 	private ViewController viewController;
@@ -14,5 +30,30 @@ public class AddDatabaseController {
 	public void setViewController(ViewController viewController) {
 		this.viewController = viewController;
 	}
+	
+	@FXML
+	private void createOutput() {
+		
+	}
+	
+	/**
+	 * Field updates when a file is loaded
+	 */
+	@FXML
+	private void loadedFileNameField() {
+		// TODO
+		
+	}
+	
+	/**
+	 * Loades the selected file
+	 */
+	@FXML
+	private void fileChooser() {
+		private Desktop desktop = Desktop.getDesktop();
+		fileChooser.showOpenDialog(stage);
+		
+	}
+	
 
 }
---
+++ javaApp/src/models/Database.java
@@ -0,0 +1,78 @@
+package models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Main.MainApp;
+
+public class Database {
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	// JDBC driver name and database URL
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost/Nationen-Landbruksmakt?useSSL=false";
+	
+	// Database credentials
+	static final String USER = "admin";
+	static final String PASS = "password"; 
+	
+	private final String dbName = "Landbruksmakt"; 
+	private MainApp mainApp;
+	
+	/**
+	 * Create the database object
+	 */
+	public Database() {
+		
+		try {
+			try {
+				// Register JDBC driver in program
+				Class.forName(JDBC_DRIVER);
+			} catch (ClassNotFoundException e) {
+				System.out.println("ERROR: can't find MYSQL JDBC Driver");
+				e.printStackTrace();
+				return;
+			}
+			
+			// Open a connection
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			String query = "CREATE DATABASE IF NOT EXIST " + dbName + ";";
+			pstmt = conn.prepareStatement(query);
+			pstmt.executeUpdate();
+		} catch(SQLException se) {
+			// Handle errors for DriverManager.getConnection() (JDBC)
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			try {
+				if (this.pstmt != null) {
+					this.pstmt.close();
+				} 
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+			
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+	}
+
+}
