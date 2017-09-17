23f67227549329f8932ba8a5a667c1ac3980ca30
Dora Oline Eriksrud <doraoline@gmail.com>
2017-09-17 12:32:38 +0200

    Started with javaApp.

---
+++ javaApp/src/Main/MainApp.java
@@ -0,0 +1,68 @@
+
+package Main;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("JavaApplication");
+		
+		initRootLayout();
+		
+		showStartScreen();
+	}
+	
+	/**
+	 * Initializes the root layout
+	 * @param args
+	 */
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void showStartScreen() {
+		try {
+			// Load start screen
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/StartScreen.fxml"));
+			AnchorPane startScreen = (AnchorPane) loader.load();
+			
+			// Set startscreen in the center of root layout.
+			rootLayout.setCenter(startScreen);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+}
