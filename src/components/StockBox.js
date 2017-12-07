e56921c440925ce2651bc3a44adb9a5f1622b568
KMikalsen <KMikalsen@users.noreply.github.com>
2017-12-07 16:26:49 +0100

    Increase card height on phones

--- src/components/StockBox.js
+++ src/components/StockBox.js
@@ -12,7 +12,7 @@ export class StockBox extends React.Component {
             <tr key={stock.id}>
                 <td>{stock.company}</td>
                 <td className="rightAlign">{( stock.numStocks ).toLocaleString( 'no-NO' )}</td>
-                <td className="rightAlign">{( "N/A" )},-</td>
+                {/* <td className="rightAlign">{( "N/A" )},-</td> */}
             </tr>
         ) );
         return listItems;
@@ -25,7 +25,7 @@ export class StockBox extends React.Component {
                     <tr>
                         <th>Selskap</th>
                         <th className="rightAlign">Aksjer</th>
-                        <th className="rightAlign">Verdi (NOK)</th>
+                        {/* <th className="rightAlign">Verdi (NOK)</th> */}
                     </tr>
                 </thead>
                 <tbody>{this.updateStocks()}</tbody>
